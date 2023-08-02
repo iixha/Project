41f8b3312ccb48625dc5c4b9f5c0b0572e303c56
alaasairafi7 <alaasairafi7@gmail.com>
2023-08-02 11:25:32 +0300

    app after adding the home page

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        var tex_tv = findViewById<TextView>(R.id.tV2)
-        var tex_nama = findViewById<EditText>(R.id.text_Name)
-        var start_botton = findViewById<Button>(R.id.start_button)
 
+    class MainActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+        }
 
 
     }
-}
--- app/src/main/java/com/example/project/MainActivity3.kt
+++ app/src/main/java/com/example/project/MainActivity3.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity3 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
